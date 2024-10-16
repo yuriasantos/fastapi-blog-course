a035f0e5e56fab644dd554ab67cb923e1d76b613
yuri santos <santosyurial@gmail.com>
2024-10-16 14:16:22 -0300

    Include main.py and README file.

---
+++ backend/core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_TITLE: str = 'Blog'
+    PROJECT_VERSION: str = '0.1.0'
+
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from core.config import settings
 
-app = FastAPI(title='Blog', version='0.1.0')
+app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 @app.get('/')
 def hello():
