8f2e41f63a2bfdd821f405b4d22daf3331f070b4
yuri santos <santosyurial@gmail.com>
2024-10-21 10:43:42 -0300

    Add authentication feature to restrict blog creation, update and deletion.

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 from db.session import get_db
+from db.models.user import User
+from apis.v1.route_login import get_current_user
 from schemas.blog import CreateBlog, ShowBlog, UpdateBlog
 from db.repository.blog import create_new_blog, retrieve_blog, list_blogs, update_blog_by_id, delete_blog_by_id
 from typing import List
@@ -9,8 +11,8 @@ from typing import List
 router = APIRouter()
 
 @router.post(path='/', response_model=ShowBlog, status_code=status.HTTP_201_CREATED)
-def create_blog(blog: CreateBlog, db: Session = Depends(get_db)):
-    blog = create_new_blog(blog=blog, db=db, author_id=1)
+def create_blog(blog: CreateBlog, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    blog = create_new_blog(blog=blog, db=db, author_id=current_user.id)
     return blog
 
 @router.get('/{id}', response_model=ShowBlog)
@@ -26,15 +28,18 @@ def get_all_blogs(db: Session = Depends(get_db)):
     return blogs
 
 @router.put('/{id}', response_model=ShowBlog)
-def update_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db)):
-    blog = update_blog_by_id(id=id, blog=blog, db=db)
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found.")
+def update_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    blog = update_blog_by_id(id=id, blog=blog, db=db, author_id=current_user.id)
+    if isinstance(blog, dict):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=blog.get('error')
+        )
     return blog
 
 @router.delete('/{id}')
-def delete_blog(id: int, db: Session = Depends(get_db)):
-    message = delete_blog_by_id(id=id, db=db, author_id=1)
+def delete_blog(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    message = delete_blog_by_id(id=id, db=db, author_id=current_user.id)
     if message.get('error'):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message.get('error'))
     else:
--- backend/db/repository/blog.py
+++ backend/db/repository/blog.py
@@ -3,7 +3,7 @@ from schemas.blog import CreateBlog, UpdateBlog
 from db.models.blog import Blog
 
 
-def create_new_blog(blog: CreateBlog, db: Session, author_id: int = 1):
+def create_new_blog(blog: CreateBlog, db: Session, author_id: int):
     blog = Blog(title=blog.title,
                 slug=blog.slug,
                 content=blog.content,
@@ -26,10 +26,12 @@ def list_blogs(db: Session):
     blogs = db.query(Blog).filter(Blog.is_active==True).all()
     return blogs
 
-def update_blog_by_id(id: int, blog:UpdateBlog, db: Session, author_id: int = 1):
+def update_blog_by_id(id: int, blog:UpdateBlog, db: Session, author_id: int):
     existing_blog = db.query(Blog).filter(Blog.id == id).first()
     if not existing_blog:
-        return None
+        return {'error': f'Blog with id {id} not found.'}
+    if not existing_blog.author_id == author_id:
+        return {'error': 'Not authorized to update this blog.'}
     existing_blog.title = blog.title
     existing_blog.content = blog.content
     try:
@@ -40,10 +42,13 @@ def update_blog_by_id(id: int, blog:UpdateBlog, db: Session, author_id: int = 1)
         db.rollback()
     return existing_blog
 
-def delete_blog_by_id(id: int, db: Session, author_id: int = 1):
+def delete_blog_by_id(id: int, db: Session, author_id: int):
     blog = db.query(Blog).filter(Blog.id == id)
     if not blog.first():
         return {'error': f'Could not find a blog with id {id}.'}
-    blog.delete()
-    db.commit()
-    return {'message': f'Successfully deleted blog with id {id}.'}
+    elif not blog.first().author_id == author_id:
+        return {'error': 'Not authorized to delete this blog.'}
+    else:
+        blog.delete()
+        db.commit()
+        return {'message': f'Successfully deleted blog with id {id}.'}
